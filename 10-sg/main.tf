de5130ecedec427f95b62b9f9efceb3d12ba627c
Mani Kanta <konugantimanikantareddy@gmail.com>
2024-07-21 14:28:09 +0530

    Added some text

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -105,7 +105,7 @@ resource "aws_security_group_rule" "node_vpc" {
   from_port         = 0
   to_port           = 65535
   protocol          = "-1" # All traffic
-  cidr_blocks = ["10.0.0.0/16"]
+  cidr_blocks = ["10.0.0.0/16"] 
   security_group_id = module.node.sg_id
 }
 
